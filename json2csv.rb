16f88c656e8850c8afdc92d7c149f7da2aec2c72
Kyle Oba <koba@pasdechocolat.com>
2013-10-18 14:52:41 -1000

    Add new version of script and input files.

--- json2csv.rb
+++ json2csv.rb
@@ -2,13 +2,40 @@
 require 'csv'
 require 'json'
 
-CSV.open("birth.csv", "w") do |csv| #open new file for write
-  is_first_row = true
-  JSON.parse(File.open("birth.json").read).each do |hash| #open json to parse
-    if is_first_row
-      csv << hash.keys
-      is_first_row = false
+INPUT_DIR = "INPUT"
+OUTPUT_DIR = "OUTPUT"
+
+def convertToCSVFromJSON(json_name)
+  CSV.open("#{OUTPUT_DIR}/#{json_name}.csv", "w") do |csv| #open new file for write
+    JSON.parse(File.open("#{INPUT_DIR}/#{json_name}.json").read).each do |hash| #open json to parse
+      results = hash[1]
+
+      # write column headers
+      csv << results.first.keys
+
+      # write each row
+      results.each do |row|
+        csv << row.values #write value to file
+      end
     end
-    csv << hash.values #write value to file
   end
+end
+
+# names = [
+#   "Avatar",
+#   "Background",
+#   "Badges",
+#   "Balances", 
+#   "GlobalStatus",
+#   "NewBalances",
+#   "NewStory",
+#   "Transactions",
+#   "XPTransactions"
+# ]
+names = [
+  "Transactions"
+]
+
+names.each do |name|
+  convertToCSVFromJSON name
 end
